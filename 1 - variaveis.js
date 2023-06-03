45b5717c4c0c6fe2130d37347aac73f297bccce1
Renally25 <renallyaciole@gamil.com>
2023-06-03 18:24:40 -0300

    adicionando codigos javascript

---
+++ 1 - variaveis.js
@@ -0,0 +1,9 @@
+var name = "Vini"
+const color = 'red'
+const media = 7
+const listaDeCompras = ['feijão', 'arroz', 'carne']
+
+console.log(name);
+console.log(color);
+console.log(media);
+console.log(listaDeCompras);
